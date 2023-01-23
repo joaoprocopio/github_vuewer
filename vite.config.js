ea5833090f96eecd773f68baf7f525110084380e
joaoprocopio <joao.procopio@buser.com.br>
2023-01-23 17:54:21 -0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import vue from "@vitejs/plugin-vue"
+
+import { fileURLToPath, URL } from "node:url"
+import { defineConfig } from "vite"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "~": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": fileURLToPath(new URL("./", import.meta.url)),
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+  base: "/githubsearch/",
+  publicDir: "./src/public/",
+})
