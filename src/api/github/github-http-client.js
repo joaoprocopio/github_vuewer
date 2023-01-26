3dc0ab4da5dd19e1364ecf2c6b6889607a288715
joaoprocopio <joao.procopio@buser.com.br>
2023-01-25 22:41:01 -0300

    v1 miragejs mockapi

--- src/api/github/github-http-client.js
+++ src/api/github/github-http-client.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+const isDevelopment = process.env.NODE_ENV === "development"
+
 const githubHttpClient = axios.create({
-  baseURL: "https://api.github.com/",
+  baseURL: isDevelopment ? undefined : "https://api.github.com/",
   timeout: 60000,
 })
 
--- src/app/index.js
+++ src/app/index.js
@@ -3,6 +3,11 @@ import App from "./App.vue"
 import { createApp } from "vue"
 
 import { installPlugins } from "~/plugins"
+import { mockServer } from "~/api"
+
+if (process.env.NODE_ENV === "development") {
+  mockServer()
+}
 
 const app = createApp(App)
 
