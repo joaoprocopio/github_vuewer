2bc0b0200c9af75e0808e98a45cd902048c4b964
joaoprocopio <joao.procopio@buser.com.br>
2023-01-25 23:02:49 -0300

    remove timeout

--- src/api/github/github-http-client.js
+++ src/api/github/github-http-client.js
@@ -3,8 +3,7 @@ import axios from "axios"
 import { env } from "@/configs"
 
 const githubHttpClient = axios.create({
-  baseURL: env.isMock ? undefined : env.apiUrl,
-  timeout: 60000,
+  baseURL: env.isDev && env.isMock ? undefined : env.apiUrl,
 })
 
 export { githubHttpClient }
