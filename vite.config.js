3fec1463efd00a455f4e5321977964ab565dc46c
joaoprocopio <joao.procopio@buser.com.br>
2023-01-25 22:38:46 -0300

    vite arrow function

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,23 @@ import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vuetify()],
-  resolve: {
-    alias: {
-      "~": fileURLToPath(new URL("./src", import.meta.url)),
-      "@": fileURLToPath(new URL("./", import.meta.url)),
+export default defineConfig(() => {
+  return {
+    plugins: [vue(), vuetify()],
+    resolve: {
+      alias: {
+        "~": fileURLToPath(new URL("./src", import.meta.url)),
+        "@": fileURLToPath(new URL("./", import.meta.url)),
+      },
     },
-  },
-  build: {
-    sourcemap: true,
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-  },
-  base: "/githubsearch/",
-  publicDir: "./src/public/",
+    build: {
+      sourcemap: true,
+    },
+    server: {
+      host: "0.0.0.0",
+      port: 3000,
+    },
+    base: "/githubsearch/",
+    publicDir: "./src/public/",
+  }
 })
