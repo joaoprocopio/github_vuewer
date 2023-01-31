6b9a1cfc314ab8fdb9255a702e7af5e4d6959b5b
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-01-31 19:18:16 -0300

    começa a mecher nas paginas de erro

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -4,6 +4,8 @@
   <VMain>
     <slot />
   </VMain>
+
+  <VFooter />
 </template>
 
 <script setup>
--- src/pages/ErrorPage/ErrorPage.vue
+++ src/pages/ErrorPage/ErrorPage.vue
@@ -2,9 +2,12 @@
   <VResponsive
     class="mx-auto pt-12 px-8"
     max-width="600px">
-    Sorry, but the page
-    <VCode class="d-inline-flex">{{ $route.fullPath }}</VCode> could not be
-    found in our servers.
+    <h1 class="font-weight-bold mb-6">Page not found</h1>
+    <p>
+      Sorry, but the page
+      <VCode class="d-inline-flex">{{ $route.path }}</VCode> could not be found
+      in our servers.
+    </p>
   </VResponsive>
 </template>
 
