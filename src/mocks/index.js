d0333765163ad55eec655474efdc78f65af1badc
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-02-15 18:50:07 -0300

    export

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -1,1 +1,26 @@
-export * from "./server"
+import { createServer, Model } from "miragejs"
+
+export const mockServer = ({ environment = "development" } = {}) => {
+  let server = createServer({
+    environment,
+
+    models: {
+      user: Model,
+    },
+
+    seeds(server) {
+      server.create("user", { username: "joaoprocopio" })
+      server.create("user", { username: "alliceprocopio" })
+    },
+
+    routes() {
+      this.get("users/:username", (schema, request) => {
+        const username = request.params.username
+
+        return schema.users.findBy({ username: username })
+      })
+    },
+  })
+
+  return server
+}
